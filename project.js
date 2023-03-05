c78b9fe7d5f3a8815b3b99b35820e679f706f2c9
TriDevelopers <ndmt.2503@gmail.com>
2023-03-05 15:26:09 -0600

    Slot Machine Project

---
+++ project.js
@@ -0,0 +1,7 @@
+// 1. Deposit some money
+// Determine number of lines to bet on
+// 2. Collect a bet amount
+// Spin the slot machine
+// Check if the player won
+// Give the player their winnings
+// Play again?
